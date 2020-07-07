270ad89bafc91299365d43db1fdf7683338428ae
Christian <christian.richter1987@gmail.com>
2020-07-07 08:27:15 +0200

    Init

--- src/de/thunderfrog/Main.java
+++ src/de/thunderfrog/Main.java
@@ -1,23 +1,22 @@
 package de.thunderfrog;
 
-import de.thunderfrog.main.init;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-
     // Arrays
-    public static String[] sArray;
-    public static Integer[] iArray;
+    String[] sArray;
+    Integer[] iArray;
 
     // ArrayList
-    public static ArrayList<String> alString = new ArrayList<String>();
-    public static ArrayList<Integer> alInteger = new ArrayList<Integer>();
+    ArrayList<String> alString = new ArrayList<String>();
+    ArrayList<Integer> alInteger = new ArrayList<Integer>();
 
-    public static void main(String[] args) throws IOException {
-        // Init App
-        init.startApp();
-        init.drawOption();
+    // Helper Variable
+    Integer i = 0;
+    String appName = "IfLoopArray";
+    String appVersion = "1.0.0";
+
+    public static void main(String[] args) {
+
     }
 }
