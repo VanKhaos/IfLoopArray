ae22ca0090c05616bf24c7e80f0b774917300e31
VanKhaos <39248582+VanKhaos@users.noreply.github.com>
2020-07-07 08:38:28 +0200

    Add files via Upload (Init)

---
+++ src/de/thunderfrog/Main.java
@@ -0,0 +1,22 @@
+package de.thunderfrog;
+
+import java.util.ArrayList;
+
+public class Main {
+    // Arrays
+    String[] sArray;
+    Integer[] iArray;
+
+    // ArrayList
+    ArrayList<String> alString = new ArrayList<String>();
+    ArrayList<Integer> alInteger = new ArrayList<Integer>();
+
+    // Helper Variable
+    Integer i = 0;
+    String appName = "IfLoopArray";
+    String appVersion = "1.0.0";
+
+    public static void main(String[] args) {
+
+    }
+}
